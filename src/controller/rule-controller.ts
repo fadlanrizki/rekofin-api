c74bed45ce7357cbd1391762c17ad700e958c2cf
fadlanrizky19 <fadlanrizky19@gmail.com>
2025-09-29 11:34:27 +0700

    feat: api list, add, delete, findby id RULE

---
+++ src/controller/rule-controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response, NextFunction } from "express";
+import { RuleService } from "../service/rule-service";
+import { TAddRule, TParamRule } from "../model/rule-model";
+
+export class RuleController {
+  static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request = req.body as unknown as TAddRule;
+      const response = await RuleService.create(request);
+      res.status(200).json({
+        ok: true,
+        data: response,
+        message: "Berhasil Menambahkan Rule",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async getList(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request = req?.query as unknown as TParamRule;
+
+      const response = await RuleService.getList(request);
+      res.status(200).json({
+        ok: true,
+        ...response,
+        message: "Berhasil Get List Rule",
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = req.params?.id;
+      const response = await RuleService.delete(id);
+      res.status(200).json({
+        ok: true,
+        data: response.data,
+        message: `Berhasil Delete Rule ${response.data.id}`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  static async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = req.params?.id;
+      const data = await RuleService.findById(id);
+      res.status(200).json({
+        ok: true,
+        data,
+        message: `Berhasil Get data Rule`,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
---
+++ src/route/rule-route.ts
@@ -0,0 +1,10 @@
+import { RuleController } from './../controller/rule-controller';
+import { Router } from "express";
+
+const ruleRouter = Router();
+ruleRouter.post("/", RuleController.create);
+ruleRouter.get("/", RuleController.getList);
+ruleRouter.get("/:id", RuleController.findById);
+ruleRouter.delete("/:id", RuleController.delete);
+
+export default ruleRouter;
