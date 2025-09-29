3926feec4dbf437ef6aa69934c8d96e3c25a9a80
fadlanrizky19 <fadlanrizky19@gmail.com>
2025-09-29 16:55:29 +0700

    fix: add rule route

--- src/application/app.ts
+++ src/application/app.ts
@@ -4,6 +4,7 @@ import authRouter from "../route/auth.route";
 import { errorMiddleware } from "../middleware/error-middleware";
 import { authenticateToken } from "../middleware/auth-middleware";
 import userRouter from "../route/user-route";
+import ruleRouter from "../route/rule-route";
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/user", authenticateToken, userRouter);
+app.use("/api/rule", authenticateToken, ruleRouter)
 app.use(errorMiddleware);
 
 app.listen(PORT, () => {
