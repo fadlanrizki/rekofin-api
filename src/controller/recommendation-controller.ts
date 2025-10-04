6ce72615d72f8f4b0d5bd68bc256044317bc9b72
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2025-10-04 13:51:56 +0700

    fix: recommendation api

--- src/controller/recommendation-controller.ts
+++ src/controller/recommendation-controller.ts
@@ -1,61 +1,64 @@
 import { Request, Response, NextFunction } from "express";
 import { RecommendationService } from "../service/recommendation-service";
-import {TAddRecommendation, TParamRecommendation} from "../model/recommendation-model";
+import {
+  TAddRecommendation,
+  TParamRecommendation,
+} from "../model/recommendation-model";
 
 export class RecommendationController {
-    static async create(req: Request, res: Response, next: NextFunction) {
-        try {
-            const request = req.body as unknown as TAddRecommendation;
-            const response = await RecommendationService.create(request);
-            res.status(200).json({
-                ok: true,
-                data: response,
-                message: "Berhasil Menambahkan Rekomendasi",
-            });
-        } catch (error) {
-            next(error);
-        }
+  static async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request = req.body as unknown as TAddRecommendation;
+      const response = await RecommendationService.create(request);
+      res.status(200).json({
+        ok: true,
+        data: response,
+        message: "Berhasil Menambahkan Rekomendasi",
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async getList(req: Request, res: Response, next: NextFunction) {
-        try {
-            const request = req.params as unknown as TParamRecommendation;
-            const response = await RecommendationService.getList(request);
-            res.status(200).json({
-                ok: true,
-                data: response,
-                message: "Berhasil mendapatkan daftar Rekomendasi",
-            });
-        } catch (error) {
-            next(error);
-        }
+  static async getList(req: Request, res: Response, next: NextFunction) {
+    try {
+      const request = req.query as unknown as TParamRecommendation;
+      const response = await RecommendationService.getList(request);
+      res.status(200).json({
+        ok: true,
+        data: response,
+        message: "Berhasil mendapatkan daftar Rekomendasi",
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async delete(req: Request, res: Response, next: NextFunction) {
-        try {
-            const id = req.params.id;
-            const response = await RecommendationService.delete(id);
-            res.status(200).json({
-                ok: true,
-                data: response,
-                message: "Berhasil delete data Rekomendasi",
-            });
-        } catch (error) {
-            next(error);
-        }
+  static async delete(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = req.params.id;
+      const response = await RecommendationService.delete(id);
+      res.status(200).json({
+        ok: true,
+        data: response,
+        message: "Berhasil delete data Rekomendasi",
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 
-    static async findById(req: Request, res: Response, next: NextFunction) {
-        try {
-            const id = req.params.id;
-            const response = await RecommendationService.findById(id);
-            res.status(200).json({
-                ok: true,
-                data: response,
-                message: `Berhasil mendapatkan data rekomendasi`,
-            });
-        } catch (error) {
-            next(error);
-        }
+  static async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const id = req.params.id;
+      const response = await RecommendationService.findById(id);
+      res.status(200).json({
+        ok: true,
+        data: response,
+        message: `Berhasil mendapatkan data rekomendasi`,
+      });
+    } catch (error) {
+      next(error);
     }
+  }
 }
--- src/service/recommendation-service.ts
+++ src/service/recommendation-service.ts
@@ -1,96 +1,100 @@
-import {prismaClient} from "../application/database";
+import { prismaClient } from "../application/database";
 import {
-    TAddRecommendation, TParamRecommendation
+  TAddRecommendation,
+  TParamRecommendation,
 } from "../model/recommendation-model";
-import {RecommendationCategory, SourceType} from "../generated/prisma";
-import {ResponseError} from "../error/response-error"
+import { RecommendationCategory, SourceType } from "../generated/prisma";
+import { ResponseError } from "../error/response-error";
 
 export class RecommendationService {
-    static async create(request: TAddRecommendation): Promise<any> {
-        const registerRequest = request as unknown as TAddRecommendation;
+  static async create(request: TAddRecommendation): Promise<any> {
+    const registerRequest = request as unknown as TAddRecommendation;
 
-        return prismaClient.recommendation.create({
-            data: {...registerRequest}
-        });
-    }
+    return prismaClient.recommendation.create({
+      data: { ...registerRequest },
+    });
+  }
 
-    static async getList(request: TParamRecommendation): Promise<any> {
-        const validRequest = request as unknown as TParamRecommendation;
+  static async getList(request: TParamRecommendation): Promise<any> {
+    const validRequest = request as unknown as TParamRecommendation;
 
-        const page = parseInt(validRequest.page);
-        const limit = parseInt(validRequest.limit);
-        const search = validRequest.search;
+    const page = parseInt(validRequest.page);
+    const limit = parseInt(validRequest.limit);
+    const search = validRequest.search;
 
-        const category = validRequest.filter.category;
-        const sourceType = validRequest.filter.sourceType;
+    const category = validRequest.filter.category;
+    const sourceType = validRequest.filter.sourceType;
 
-        const searchCondition = search
-            ? {
-                OR: [
-                    {title: {contains: search}},
-                    {sourceName: {contains: search}},
-                    {author: {contains: search}}
-                ],
-            }
-            : {};
-
-        const categoryCondition = category === "all"
-            ? {category: {in: Object.values(RecommendationCategory)}}
-            : {category: category as RecommendationCategory};
-
-        const sourceTypeCondition = sourceType === "all" ?
-            {sourceType: {in: Object.values(SourceType)}}
-            : {sourceType: sourceType as SourceType}
-
-        const data = await prismaClient.recommendation.findMany({
-            skip: (page - 1) * limit,
-            take: limit,
-            where: {
-                AND: [searchCondition, categoryCondition, sourceTypeCondition]
-            },
-            orderBy: {
-                createdAt: "desc"
-            }
-        });
-
-        const total = await prismaClient.recommendation.count();
-
-        return {
-            data,
-            total,
-            page
+    const searchCondition = search
+      ? {
+          OR: [
+            { title: { contains: search } },
+            { sourceName: { contains: search } },
+            { author: { contains: search } },
+          ],
         }
+      : {};
+
+    const categoryCondition =
+      category === "all"
+        ? { category: { in: Object.values(RecommendationCategory) } }
+        : { category: category as RecommendationCategory };
+
+    const sourceTypeCondition =
+      sourceType === "all"
+        ? { sourceType: { in: Object.values(SourceType) } }
+        : { sourceType: sourceType as SourceType };
+
+    const data = await prismaClient.recommendation.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+      where: {
+        AND: [searchCondition, categoryCondition, sourceTypeCondition],
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const total = await prismaClient.recommendation.count({
+      where: {
+        AND: [searchCondition, categoryCondition, sourceTypeCondition],
+      },
+    });
+
+    return {
+      data,
+      total,
+      page,
+    };
+  }
+
+  static async delete(id: string): Promise<any> {
+    const selectedId = parseInt(id);
+    await prismaClient.recommendation.delete({
+      where: {
+        id: selectedId,
+      },
+    });
+  }
+
+  static async findById(id: string): Promise<any> {
+    const selectedId = parseInt(id);
+
+    const selectCount = await prismaClient.recommendation.count({
+      where: {
+        id: selectedId,
+      },
+    });
+
+    if (!selectCount) {
+      throw new ResponseError(400, "Cannot Get Data, ID not found");
     }
 
-    static async delete(id: string): Promise<any> {
-        const selectedId = parseInt(id);
-        await prismaClient.recommendation.delete({
-            where: {
-                id: selectedId
-            },
-            select: {
-                title: true
-            }
-        })
-    }
-
-    static async findById(id: string): Promise<any> {
-        const selectedId = parseInt(id);
-
-        const selectCount = await prismaClient.recommendation.count({
-            where: {
-                id: selectedId
-            }
-        });
-
-        if (!selectCount) {
-            throw new ResponseError(400, "Cannot Get Data, ID not found");
-        }
-
-        return await prismaClient.recommendation.findUnique({
-            where: {
-                id: selectedId
-            }
-        })
-    }
+    return await prismaClient.recommendation.findUnique({
+      where: {
+        id: selectedId,
+      },
+    });
+  }
 }
