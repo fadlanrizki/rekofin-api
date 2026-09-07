028ef14a7aff793f9c7c205a714f7bff823646be
Fadlan Rizki <66633252+fadlanrizki@users.noreply.github.com>
2026-09-07 08:02:40 +0700

    fix: main log

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import app from "./application/app";
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server ready on http://localhost:${PORT}`);
-});
+
+if (process.env.VERCEL) {
+  console.log("🚀 Backend running on Vercel Production");
+} else {
+  app.listen(PORT, () => {
+    console.log(`🚀 Backend running locally on http://localhost:${PORT}`);
+  });
+}
